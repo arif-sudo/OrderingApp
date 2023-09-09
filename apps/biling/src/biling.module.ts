8c18e692a81aef398ef7e1985f7f8b1ff9a908ae
Arif <arif520gg@gmail.com>
2023-09-09 12:00:27 +0400

    biling ms docker

--- apps/biling/src/biling.module.ts
+++ apps/biling/src/biling.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { BilingController } from './biling.controller';
 import { BilingService } from './biling.service';
+import { RmqModule } from '@app/common/rmq/rmq.module';
+import { ConfigModule } from '@nestjs/config';
+import * as joi from 'joi'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: joi.object({
+        RMQ_URI: joi.string().required(),
+        RMQ_BILING_QUEUE: joi.string().required()
+      })
+    }),
+    RmqModule
+  ],
   controllers: [BilingController],
   providers: [BilingService],
 })
--- apps/biling/src/main.ts
+++ apps/biling/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { BilingModule } from './biling.module';
+import { RmqService } from '@app/common/rmq/rmq.service';
+import { RmqOptions } from '@nestjs/microservices';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(BilingModule);
-  await app.listen(3000);
+  const rmqService = app.get<RmqService>(RmqService)
+  // obtain an instance of the RmqService class. This function is used for dependency injection in NestJS
+  app.connectMicroservice<RmqOptions>(rmqService.getOptions('BILING'))
+
+  await app.startAllMicroservices();
 }
+
 bootstrap();
